f24911b336347aacaf2affb1bad068ae5dab68c1
ismdeep <ismdeep@protonmail.com>
2019-11-30 23:38:03 +0800

    Add centos-install-justoj.sh

---
+++ centos-install-justoj.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+yum -y update
+yum -y install make gcc gcc-c++
+yum -y install nginx
+systemctl enable nginx
+systemctl start nginx
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
+#https://blog.csdn.net/FungLeo/article/details/78789932
+yum -y install php
+
+nginx -V
+php --version
+
