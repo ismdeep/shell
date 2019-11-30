58e053fd4813b782bc0a5f4ee6b73e904593fc3e
ismdeep <ismdeep@protonmail.com>
2019-11-30 23:43:21 +0800

    Add centos-install-justoj.sh

--- centos-install-justoj.sh
+++ centos-install-justoj.sh
@@ -8,7 +8,8 @@ firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --reload
 #https://blog.csdn.net/FungLeo/article/details/78789932
-yum -y install php
+yum -y install php php-fpm php-gd php-ldap php-mbstring php-odbc php-pear php-xml php-xmlrpc
+
 
 nginx -V
 php --version
