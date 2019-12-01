988945eceff18196f5ce353a2290a09422537810
ismdeep <ismdeep@protonmail.com>
2019-12-01 18:42:56 +0800

    Add centos-install-justoj.sh

--- centos-install-justoj.sh
+++ centos-install-justoj.sh
@@ -7,9 +7,13 @@ systemctl start nginx
 firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --reload
-#https://blog.csdn.net/FungLeo/article/details/78789932
+# https://blog.csdn.net/FungLeo/article/details/78789932
+# https://www.linuxbabe.com/redhat/install-lemp-nginx-mariadb-php7-rhel-8-centos-8
 yum -y install php php-fpm php-gd php-ldap php-mbstring php-odbc php-pear php-xml php-xmlrpc
 
+systemctl start php-fpm
+systemctl enable php-fpm
+systemctl status php-fpm
 
 nginx -V
 php --version
