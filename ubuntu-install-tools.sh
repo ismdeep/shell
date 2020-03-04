dd2e2239cb621d60a28caf699f6adaed0dcf27e1
ismdeep <ismdeep@live.com>
2020-03-04 17:10:59 +0800

    Add ubuntu-install-tools.sh

---
+++ ubuntu-install-tools.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function system_update() {
+    apt-get update
+    apt-get upgrade -y
+}
+
+function install_gcc() {
+    apt-get update
+    apt-get install -y gcc g++ make
+}
+
+function install_cmake() {
+    apt-get update
+    apt-get install -y cmake
+}
+
+function install_zsh() {
+    apt-get update
+    apt-get install -y zsh git
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+    chsh -s /bin/zsh
+}
+
+system_update
+install_gcc
+install_cmake
