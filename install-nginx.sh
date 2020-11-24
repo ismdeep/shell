48dd9df0ce51f3077777f9de0c9798ef98a47e45
ismdeep <l.jiang.1024@gmail.com>
2020-11-24 22:50:00 +0800

    update

--- install-nginx.sh
+++ install-nginx.sh
@@ -5,22 +5,25 @@ command_exists() {
 }
 
 pre_install_centos() {
-    yum -y install gcc gcc-c++ automake autoconf libtool make wget
-    yum -y install libmcrypt-devel mhash-devel libxslt-devel \
-                    libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel \
-                    libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 \
-                    glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel \
-                    e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel
-    yum -y install epel-release
-    yum -y install axel
-    yum -y install gcc gcc-c++ glibc
-    yum -y install libmcrypt-devel mhash-devel \
-                    libxslt-devel libjpeg libjpeg-devel libpng \
-                    libpng-devel freetype freetype-devel libxml2 \
-                    libxml2-devel zlib zlib-devel glibc glibc-devel \
-                    glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel \
-                    curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel \
-                    libidn libidn-devel openssl openssl-devel
+    yum -y install  epel-release \
+                    gcc gcc-c++ make cmake \
+                    automake autoconf libtool  wget \
+                    libmcrypt-devel mhash-devel libxslt-devel \
+                    libjpeg libjpeg-devel \
+                    libpng libpng-devel \
+                    freetype freetype-devel \
+                    libxml2 libxml2-devel \
+                    zlib zlib-devel \
+                    glibc glibc-devel \
+                    glib2 glib2-devel \
+                    bzip2 bzip2-devel \
+                    ncurses ncurses-devel \
+                    curl curl-devel \
+                    e2fsprogs e2fsprogs-devel \
+                    krb5 krb5-devel \
+                    libidn libidn-devel \
+                    openssl openssl-devel \
+                    axel
 }
 
 
