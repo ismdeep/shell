253a89f6cd227b1915e0fbbb5e9ccf0724e3f11b
ismdeep <l.jiang.1024@gmail.com>
2020-11-24 22:55:50 +0800

    update

--- install-nginx.sh
+++ install-nginx.sh
@@ -6,24 +6,23 @@ command_exists() {
 
 pre_install_centos() {
     yum -y install  epel-release \
-                    gcc gcc-c++ make cmake \
-                    automake autoconf libtool  wget \
-                    libmcrypt-devel mhash-devel libxslt-devel \
-                    libjpeg libjpeg-devel \
-                    libpng libpng-devel \
-                    freetype freetype-devel \
-                    libxml2 libxml2-devel \
-                    zlib zlib-devel \
-                    glibc glibc-devel \
-                    glib2 glib2-devel \
-                    bzip2 bzip2-devel \
-                    ncurses ncurses-devel \
-                    curl curl-devel \
-                    e2fsprogs e2fsprogs-devel \
-                    krb5 krb5-devel \
-                    libidn libidn-devel \
-                    openssl openssl-devel \
-                    axel
+                    gcc                gcc-c++        make             cmake    \
+                    automake           autoconf       libtool          wget     \
+                    libmcrypt-devel    mhash-devel    libxslt-devel    axel     \
+                    libjpeg            libjpeg-devel \
+                    libpng             libpng-devel \
+                    freetype           freetype-devel \
+                    libxml2            libxml2-devel \
+                    zlib               zlib-devel \
+                    glibc              glibc-devel \
+                    glib2              glib2-devel \
+                    bzip2              bzip2-devel \
+                    ncurses            ncurses-devel \
+                    curl               curl-devel \
+                    e2fsprogs          e2fsprogs-devel \
+                    krb5               krb5-devel \
+                    libidn             libidn-devel \
+                    openssl            openssl-devel
 }
 
 
