c0ee1f2f2d232b3dbdc7c20098ad06ed645cb7fe
L. Jiang <l.jiang.1024@gmail.com>
2021-01-05 20:12:42 +0800

    update

--- data/run.sh
+++ data/run.sh
@@ -2,12 +2,12 @@
 
 python-list | grep -v "\.asc" | grep -v "\.chm" | sort > python.txt
 
+tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/AdoptOpenJDK/ | grep -v "\.rpm" | grep -v "\.deb" | grep -v "dists" | grep -v "filelist" | grep -v "\.xml" | sort > openjdk.txt
+
+tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/centos/7/isos/ | grep "\.iso" | sort > centos.txt
+tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/centos/8/isos/ | grep "\.iso" | sort >> centos.txt
+
 exit 0
-
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/AdoptOpenJDK/ openjdk
-
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/centos/7/isos/ centos
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/centos/8/isos/ centos --append
 
 tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/opensuse/distribution/ opensuse2
 cat opensuse2.txt | grep "\.iso" | grep -v "Changes" > opensuse.txt
