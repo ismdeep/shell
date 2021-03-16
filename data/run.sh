3449445cc5978c267de2924b139ced0748c3a8d2
L. Jiang <l.jiang.1024@gmail.com>
2021-03-17 07:19:30 +0800

    update

--- data/run.sh
+++ data/run.sh
@@ -2,21 +2,21 @@
 
 python-list | grep -v "\.asc" | grep -v "\.chm" | sort > python.txt
 
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/AdoptOpenJDK/ | grep -v "\.rpm" | grep -v "\.deb" | grep -v "dists" | grep -v "filelist" | grep -v "\.xml" | sort > openjdk.txt
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/AdoptOpenJDK/ -x ".rpm" -x ".deb" -x "dists" -x "filelist" -x ".xml" -out openjdk.txt
 
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/centos/7/isos/ | grep "\.iso" | sort > centos.txt
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/centos/8/isos/ | grep "\.iso" | sort >> centos.txt
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/centos/7/isos/ -a ".iso" -x "torrent" -x ".manifest" -out centos.txt
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/centos/8/isos/ -a ".iso" -x "torrent" -x ".manifest" -out centos.txt --append
 
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/opensuse/distribution/ | grep "\.iso" | grep -v "Changes" | sort | grep -v "\.sha256" | sort > opensuse.txt
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/opensuse/distribution/    -a ".iso"    -x "Changes"    -x ".sha256"  -out opensuse.txt
 
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/12.04/ | grep "\.iso" | grep -v "\.zsync" | sort > ubuntu.txt
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/14.04/ | grep "\.iso" | grep -v "\.zsync" | sort >> ubuntu.txt
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/ | grep "\.iso" | grep -v "\.zsync" | sort >> ubuntu.txt
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/18.04/ | grep "\.iso" | grep -v "\.zsync" | sort >> ubuntu.txt
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/20.04/ | grep "\.iso" | grep -v "\.zsync" | sort >> ubuntu.txt
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/12.04/    -a ".iso"    -x ".zsync"    -x ".torrent"    -out ubuntu.txt
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/14.04/    -a ".iso"    -x ".zsync"    -x ".torrent"    -out ubuntu.txt    --append
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/    -a ".iso"    -x ".zsync"    -x ".torrent"    -out ubuntu.txt    --append
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/18.04/    -a ".iso"    -x ".zsync"    -x ".torrent"    -out ubuntu.txt    --append
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/20.04/    -a ".iso"    -x ".zsync"    -x ".torrent"    -out ubuntu.txt    --append
 
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/solus/images/ | grep -v "\.sha256sum" | sort > solus.txt
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/solus/images/    -a "images"   -x ".sha256sum"    -out solus.txt
 
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/deepin-cd/ | grep "\.iso" | sort > deepin.txt
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/deepin-cd/    -a ".iso" -out deepin.txt
 
-tuna-dumper https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/ 2>/dev/null | grep "\.iso" | sort > fedora.txt
+tuna-dumper -url https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/    -a ".iso" -x ".manifest" -out fedora.txt
